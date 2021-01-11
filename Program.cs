c693d7405ca63dbf4890b151d01aade1092d364e
AnanyaNagar <60837177+AnanyaNagar@users.noreply.github.com>
2021-01-11 21:21:17 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey Everyone!! I am Ananya Nagar");
+            Console.WriteLine("This is my first program with C#");
+        }
+    }
+}
